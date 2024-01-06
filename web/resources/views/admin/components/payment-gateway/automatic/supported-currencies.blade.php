b84d29ed44b5c3df3057c91455d590035d972f9a
Sahil <151955025+sahilarun@users.noreply.github.com>
2024-01-06 13:50:26 +0530

    Web - Components

---
+++ web/resources/views/admin/components/payment-gateway/automatic/supported-currencies.blade.php
@@ -0,0 +1,14 @@
+@isset($gateway)
+    <div class="gateway-content text-end">
+        <h5 class="title">{{ __("Total Supported Currency") }}</h5>
+    </div>
+
+    <div class="custom-checkbox-area">
+        @foreach ($gateway->supported_currencies as $item)
+        <div class="custom-check-group two">
+            <input type="checkbox" id="{{ Str::lower($item) }}" class="payment-gateway-currency" data-currency="{{ $item }}" data-default-currency="{{ get_default_currency_code($default_currency) }}" {{ $option ?? "" }} @if ($gateway->currencies->where('currency_code',$item)->count() == 1) checked @endif>
+            <label for="{{ Str::lower($item) }}">{{ $item }}</label>
+        </div>
+        @endforeach
+    </div>
+@endisset
